6204e572ed44c565f277955111ae80ce95d46f3e
rezgardarvand <rezgardarvand@yahoo.com>
2023-04-12 21:09:56 +0430

    rename

---
+++ convert degress to UTM.R
@@ -0,0 +1,10 @@
+ # convert Degress to UTM
+angle2dec <- function(data) {
+  data <- as.character(data)
+  x <- do.call(rbind, strsplit(angle, split=' '))# do.call for creat on function againset lapply - strsplit for seperat data based on symbol like " "
+  x <- apply(x, 1L, function(y) {
+    y <- as.numeric(y)
+    y[1] + y[2]/60 + y[3]/3600 # each of seperat data calculate on hour, minutes and secound
+  })
+  return(x)
+}
